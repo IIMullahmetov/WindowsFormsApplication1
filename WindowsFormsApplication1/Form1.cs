08f3002802c64e33d39e303fbffeb40f1e4da1f5
Ильяс Муллахметов <IIMullahmetov@gmail.com>
2016-09-26 23:43:28 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form   
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string address = textBox1.Text;
+            if (Directory.Exists(address)){
+                WorkWithFolder workWithFolder = new WorkWithFolder(address);
+            }
+            else
+            {
+                MessageBox.Show("Извините, указанная вами папка не найдена");
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WorkWithFolder.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    class WorkWithFolder 
+    {
+        private string addressOfCurrentFolder; // адрес введенной нами папки
+        private string addressOfExtensions = @"Extensions\"; // относительный адрес папки с расширениями
+        private string newAddress;  // новые адреса для файлов
+
+        public WorkWithFolder(string address)
+        {
+            this.addressOfCurrentFolder = address;
+            go();
+            
+        }
+
+        private string getAddressOfFolderFromRegistry(string value) // новые адреса это папки библиотек, получаем их местонахождение из реестра
+        {
+            string myAddress = "";
+            string addressOnRegistry = @"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders";
+            try
+            {
+                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(addressOnRegistry);
+                myAddress = registryKey.GetValue(value).ToString();
+                registryKey.Close();
+            }
+            catch
+            {
+                ArgumentException e;
+            }
+            return myAddress;
+        }
+
+        private void go()
+        {
+            foreach (string name in Directory.GetFiles(addressOfExtensions)) 
+            {
+                string lineInFile;
+                StreamReader streamReader = new StreamReader(name);
+                newAddress = getAddressOfFolderFromRegistry(Path.GetFileNameWithoutExtension(name));
+                while ((lineInFile = streamReader.ReadLine()) != null)
+                {
+                    foreach (string nameOfFileWithThisExtension in Directory.GetFiles(addressOfCurrentFolder, lineInFile))
+                    {
+                        string newFileNameWithAddress = Path.Combine(newAddress, Path.GetFileName(nameOfFileWithThisExtension));
+                        File.Move(nameOfFileWithThisExtension, newFileNameWithAddress);
+                    }
+                }
+            }
+        }
+    }
+}
